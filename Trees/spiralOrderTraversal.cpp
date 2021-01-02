c67e373847c79b4f3f31c43fbf87022264865f65
Rishabh Patel <nishurishabh@gmail.com>
2021-01-02 19:28:49 +0530

    Added spiral traversaL

---
+++ Trees/spiralOrderTraversal.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode {
+    public: 
+        int val;
+        TreeNode* left;
+        TreeNode* right;
+        TreeNode(int x)
+        {
+            val=x;
+            left=nullptr;
+            right=nullptr;
+        }
+};
+
+void printArray(vector<int> v, bool c)
+{
+    if(c) 
+        for(auto x: v) cout<< x <<" ";
+    else 
+    {
+        for(int j=v.size()-1; j>=0; j--)
+            cout<< v[j]<< " ";
+    }
+}
+void spiralTraversal(TreeNode* root)
+{
+    queue<TreeNode*> q;
+    bool counter=true;
+    q.push(root);
+    while(!q.empty())
+    {
+        vector<int> v;
+        int size = q.size();
+        for(int i=0; i<size; i++)
+        {
+            TreeNode* temp = q.front();
+            q.pop();
+            v.push_back(temp->val);
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }
+        printArray(v,counter);
+        counter=!counter;
+    }
+}
+void inorder(TreeNode* root)
+{
+    if(root == nullptr) return;
+    inorder(root->left);
+    cout<< root->val;
+    inorder(root->right);
+}
+
+int main()
+{
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+    root->right->left->left = new TreeNode(8);
+    root->right->right->left = new TreeNode(9);
+    spiralTraversal(root);
+}
+
+// Tree: 1,2,3,4,5,6,7
+// Output: 1 2 3 7 6 5 4
